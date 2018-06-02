7ddc8b49ac10dded13bd512dfb9ebe111f9bde4d
williammaich <williammaich@hotmail.com>
2018-06-02 15:14:29 -0300

    producao com botoes pronto

---
+++ HomeFarmSD/HomeFarmSD/ProducaoSuinosUserControl1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace HomeFarmSD
+{
+    partial class ProducaoSuinosUserControl1
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Suinos";
+            // 
+            // ProducaoSuinosUserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label1);
+            this.Name = "ProducaoSuinosUserControl1";
+            this.Size = new System.Drawing.Size(667, 384);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
--- HomeFarmSD/HomeFarmSD/ProducaoUserControl1.cs
+++ HomeFarmSD/HomeFarmSD/ProducaoUserControl1.cs
@@ -16,5 +16,30 @@ namespace HomeFarmSD
         {
             InitializeComponent();
         }
+
+        private void btnProducaoCampos_Click(object sender, EventArgs e)
+        {
+            producaoCamposUserControl11.BringToFront();
+        }
+
+        private void btnProducaoBovinos_Click(object sender, EventArgs e)
+        {
+            producaoBovinosUserControl11.BringToFront();
+        }
+
+        private void btnProducaoEquinos_Click(object sender, EventArgs e)
+        {
+            producaoEquinosUserControl11.BringToFront();
+        }
+
+        private void btnProducaoOvinos_Click(object sender, EventArgs e)
+        {
+            producaoOvinosUserControl11.BringToFront();
+        }
+
+        private void btnProducaoSuinos_Click(object sender, EventArgs e)
+        {
+            producaoSuinosUserControl11.BringToFront();
+        }
     }
 }
